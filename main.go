ca51f094177d4136d8c121a53b38ea7d7105c639
Dmitri Shuralyov <shurcooL@gmail.com>
2013-12-09 23:45:52 -0800

    Skip doing remote work for repeated git repos.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"runtime"
 	"strings"
+	"sync"
 
 	. "gist.github.com/7480523.git"
 	. "gist.github.com/7651991.git"
@@ -13,11 +14,27 @@ import (
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
+	var lock sync.Mutex
+	checkedGitRepos := map[string]bool{}
+
 	reduceFunc := func(in string) interface{} {
 		if x := SomethingFromImportPath(in); x != nil {
 			Standard := x.Bpkg.Goroot && x.Bpkg.ImportPath != "" && !strings.Contains(x.Bpkg.ImportPath, ".")
 
 			if !Standard {
+				// HACK: Check that the same git repo hasn't already been done
+				if isGitRepo, rootPath := GetGitRepoRoot(x.Path); isGitRepo {
+					lock.Lock()
+					if !checkedGitRepos[rootPath] {
+						checkedGitRepos[rootPath] = true
+						lock.Unlock()
+					} else {
+						lock.Unlock()
+						// TODO: Instead of skipping git repos that were done, cache their state and reuse it
+						return "@---- " + x.Bpkg.ImportPath
+					}
+				}
+
 				x.Update()
 				return x.String()
 			}
